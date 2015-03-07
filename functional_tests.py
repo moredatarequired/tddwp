134ef25dafdd3de4afd13b5a6588153be9c193b5
Hugh Wimberly <hugh@udacity.com>
2015-03-06 20:04:33 -0800

    Add a test to look for to-do list info.

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,44 @@
+import unittest
+
 from selenium import webdriver
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out it's homepage
+        self.browser.get('http://localhost:8000')
+
+        # She notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+        # She is invited to enter a to-do item straight away
+
+        # She types "Buy peacock feathers" into a text box
+
+        # When she hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list
+
+        # There is still a text box inviting her to add another item. She
+        # entters "Make a lure"
+
+        # The page updates again, and now shows both items on her list
+
+        # The site generates a unique URL for her, and explains she can
+        # go there to see her list any time
+
+        # She visits that URL - her to-do list is still there
+
+        # She leaves
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')        
